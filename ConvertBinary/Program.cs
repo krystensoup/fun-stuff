bebbb44a8a6dde49d5169b3d497f1ce6811bec57
krystensoup <143670948+krystensoup@users.noreply.github.com>
2023-09-07 16:55:32 -0400

    create + initial work
    basic, basic framework for the program
    IT DOES NOT WORK (I started and could not finish the framework before saving)

---
+++ ConvertBinary/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Krysten Guerrero
+ * RIT student
+ * 9/7/23           */
+
+using System.Runtime.CompilerServices;
+
+namespace ConvertBinary
+{
+    internal class Program
+    {
+        //creates all variables needed for this program
+        static private string userConvertInput = "", decimalNumber = "", binaryNumber = "";
+
+        static void Main(string[] args)
+        {
+            //ask for user input
+            Console.WriteLine("Type 1 to convert decimal to binary");
+            Console.WriteLine("Type 2 to convert binary to decimal");
+            userConvertInput = Console.ReadLine();
+
+            //make sure program doesn't break!!
+            if (userConvertInput.Equals("1"))
+                DecimalToBinary();
+            else if (userConvertInput.Equals("2"))
+                BinaryToDecimal();
+            else
+            {
+                Console.WriteLine("That is not an option");
+
+                /* HEY HEY
+                 * FIX THIS LATER
+                 * NEED TO MAKE PROGRAM NOT RESTART ANYTIME SOMEONE INPUTS SMTH WRONG
+                 * */
+                Console.WriteLine("Press any key to restart program");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        static void DecimalToBinary()
+        {
+            int 
+            Console.WriteLine("What decimal number would you like to convert?");
+            Console.WriteLine("Please note that the highest number this program can convert atm is 63");
+            Console.WriteLine("Please note that this program cannot convert negative numbers yet")
+            decimalNumber = Console.ReadLine();
+
+
+        }
+
+        static void BinaryToDecimal()
+        {
+
+        }
+    }
+}
