22a05e1a514913f8985660a1efa3f77b6da15605
krystensoup <143670948+krystensoup@users.noreply.github.com>
2023-09-08 09:22:22 -0400

    Added Program Checks
    added method that makes sure user doesn't input things that the program can't handle

--- ConvertBinary/Program.cs
+++ ConvertBinary/Program.cs
@@ -1,6 +1,10 @@
 ﻿/* Krysten Guerrero
  * RIT student
- * 9/7/23           */
+ * 9/7/23
+ * 
+ * THIS PROGRAM WAS MADE TO HELP ME FAMILIARIZE MYSELF WITH C#
+ * THIS PROGRAM WAS NOT MADE TO CONVERT LARGE NUMBERS
+ */
 
 using System.Runtime.CompilerServices;
 
@@ -9,20 +13,33 @@ namespace ConvertBinary
     internal class Program
     {
         //creates all variables needed for this program
-        static private string userConvertInput = "", decimalNumber = "", binaryNumber = "";
+        static private string optionInput = "";
+        static private int numberToConvert = 0;
 
         static void Main(string[] args)
         {
+            //warning on program limits
+            Console.WriteLine("Please note that the highest number this program can convert atm is 127");
+            Console.WriteLine("Please note that this program cannot convert negative numbers yet");
+            Console.WriteLine();
+
             //ask for user input
             Console.WriteLine("Type 1 to convert decimal to binary");
             Console.WriteLine("Type 2 to convert binary to decimal");
-            userConvertInput = Console.ReadLine();
+            optionInput = Console.ReadLine();
+            Console.WriteLine();
 
             //make sure program doesn't break!!
-            if (userConvertInput.Equals("1"))
+            if (optionInput.Equals("1"))
+            {
+                Console.WriteLine("What decimal number would you like to convert?");
+                numberToConvert = inputCheck(Console.ReadLine());
                 DecimalToBinary();
-            else if (userConvertInput.Equals("2"))
+            }
+            else if (optionInput.Equals("2"))
+            {
                 BinaryToDecimal();
+            }
             else
             {
                 Console.WriteLine("That is not an option");
@@ -39,18 +56,26 @@ namespace ConvertBinary
 
         static void DecimalToBinary()
         {
-            int 
-            Console.WriteLine("What decimal number would you like to convert?");
-            Console.WriteLine("Please note that the highest number this program can convert atm is 63");
-            Console.WriteLine("Please note that this program cannot convert negative numbers yet")
-            decimalNumber = Console.ReadLine();
-
-
+            
         }
 
         static void BinaryToDecimal()
         {
+            
+        }
 
+        static int inputCheck(string input)
+        {
+            //makes sure system doesn't break if you input smth that doesn't work in the program!
+            if(int.TryParse(input, out numberToConvert) == true)
+                if(numberToConvert < 128)
+                {
+                    Console.WriteLine(numberToConvert);
+                    return numberToConvert;
+                }
+            Console.WriteLine();
+            Console.WriteLine("That is not a valid input");
+            return 0;
         }
     }
 }
